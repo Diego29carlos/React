b97365c295d5b264c5822854a6da531328c35dca
diego29carlos <juan29-diego@hotmail.com>
2020-01-31 16:47:16 -0300

    por-terminar

--- src/App.js
+++ src/App.js
@@ -1,71 +1,72 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
- 
+
+
 function App() {
- return (
-   <div className="App">
-     <header className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-       <p>
-         <h1>J.Diego C.Carlos</h1>
-       </p>
-       <form>
- <label>
-   Nombre y Apellido: <input type="text" name="name" />
- </label>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>J.Diego C.Carlos</h1>
+        </p>
+        <form>
+
+  <ul>
+    Nombre y Apellido: <input type="text" name="name" />
+  </ul>
+  
+  <ul>
+    Correo: <input type="text" name="name" />
+  </ul>
+
+  <ul>Edad: <input type="text" name="name" />
+  </ul>
  
- <label>
-   Correo: <input type="text" name="name" />
- </label>
- <label>
+  <ul>
+    Telefono: <input type="text" name="name"/>
+  </ul>
 
-  Edad: <input type="text" name="name" />
- </label>
 
- <label>
-   Telefono: <input type="text" name="name"/>
-
- </label>
- </form>
- <input type="Submit" value="Submit" />
-     </header>
-     
-   </div>
- );
-}
- 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-  
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-  
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-  
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+  </form>
+  <input type="Submit" value="Submit" />
+      </header>
+      
+    </div>
+  );
  }
-
-export default App;
   
+ class NameForm extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {value: ''};
+   
+     this.handleChange = this.handleChange.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
+   }
+   
+   handleChange(event) {
+     this.setState({value: event.target.value});
+   }
+   
+   handleSubmit(event) {
+     alert('A name was submitted: ' + this.state.value);
+     event.preventDefault();
+   }
+   
+   render() {
+     return (
+       <form onSubmit={this.handleSubmit}>
+         <label>
+           Name:
+           <input type="text" value={this.state.value} onChange={this.handleChange} />
+         </label>
+         <input type="submit" value="Submit" />
+       </form>
+     );
+   }
+  }
+ 
+ export default App;
  
--- src/index.js
+++ src/index.js
@@ -10,3 +10,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+  
+// function home() {
+  //   return (
+    //     <div className="main-container">
+        // <imagenes />
+      //   </div>
+   //  );
+ // }
+   //  export  default home;
